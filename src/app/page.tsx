c4a799755a42b0e8e00aa6dba612bdf11bb13803
Nicolas Mayorga <namayorgav@gmail.com>
2024-08-16 12:42:43 -0500

    feat(app): installed all the necessary shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,13 @@
+import { Button } from "@/components/ui/button"
+
 function HomePage() {
-  return <h1>HomePage</h1>
+  return (
+    <div>
+      <h1 className="text-3xl">HomePage</h1>
+      <Button className="capitalize m-8" variant={"outline"} size={"lg"}>
+        Click me
+      </Button>
+    </div>
+  )
 }
 export default HomePage
