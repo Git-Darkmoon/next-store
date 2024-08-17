3f99af48e28846960fcb8c1023b17a3cb9a98e10
Nicolas Mayorga <namayorgav@gmail.com>
2024-08-17 10:04:40 -0500

    feat(navbar): created dropdown menu

--- src/components/navbar/LinksDropdown.tsx
+++ src/components/navbar/LinksDropdown.tsx
@@ -1,4 +1,35 @@
+import { AlignLeft } from "lucide-react"
+import { Button } from "../ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu"
+import { links } from "@/utils/links"
+import Link from "next/link"
+
 function LinksDropdown() {
-  return <div>LinksDropdown</div>
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" className="flex gap-4 max-w-[100px]">
+          <AlignLeft className="size-6" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-40" align="end" sideOffset={10}>
+        {links.map((link) => {
+          return (
+            <DropdownMenuItem key={link.href}>
+              <Link href={link.href} className="capitalize w-full">
+                {link.label}
+              </Link>
+            </DropdownMenuItem>
+          )
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
 }
+
 export default LinksDropdown
