06e20e4a29646afb7bdb58dfc0182de9ea6524d3
Nicolas Mayorga <namayorgav@gmail.com>
2024-08-20 23:16:50 -0500

    feat($about): add static content with all related information

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,29 @@
 function AboutPage() {
-  return <div>AboutPage</div>
+  return (
+    <section>
+      <h1 className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text-6xl">
+        We love
+        <span className="bg-primary py-2 px-4 rounded-lg tracking-wider text-white">
+          store
+        </span>
+      </h1>
+      <p className="mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto text-muted-foreground">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero hic
+        distinctio ducimus temporibus nobis autem laboriosam repellat, magni
+        fugiat minima excepturi neque, tenetur possimus nihil atque! Culpa nulla
+        labore nam?
+      </p>
+      <p className="mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto text-muted-foreground">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero hic
+        distinctio ducimus temporibus nobis autem laboriosam repellat, magni
+        fugiat minima excepturi neque, Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Accusamus incidunt non alias ex accusantium, laborum
+        impedit voluptatum dolorum eaque corporis reiciendis, tempore cum
+        doloribus illo beatae dolor distinctio iure quod nesciunt enim
+        inventore? Temporibus eius inventore voluptate a, odit ad vel, maiores
+        quos modi quaerat sapiente? Sit libero beatae eum!
+      </p>
+    </section>
+  )
 }
 export default AboutPage
