f33db179ff93f8d3bb57b8b64f54cf86b5bac9ed
Nicolas Mayorga <namayorgav@gmail.com>
2024-08-24 12:26:31 -0500

    feat($global): add auth provider and implement navbar icon

--- src/components/navbar/UserIcon.tsx
+++ src/components/navbar/UserIcon.tsx
@@ -1,4 +1,22 @@
-function UserIcon() {
-  return <div>UserIcon</div>
+import { currentUser } from "@clerk/nextjs/server"
+import { User2Icon } from "lucide-react"
+import Image from "next/image"
+
+async function UserIcon() {
+  // const { userId } = auth()
+  const user = await currentUser()
+
+  const profileImage = user?.imageUrl
+  if (profileImage)
+    return (
+      <Image
+        src={profileImage as string}
+        alt="profile image"
+        width={24}
+        height={24}
+        className="size-6 rounded-full object-cover"
+      />
+    )
+  return <User2Icon className="size-6 p-1 bg-primary rounded-full text-white" />
 }
 export default UserIcon
