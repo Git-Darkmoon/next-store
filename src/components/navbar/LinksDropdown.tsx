59d9de7c0b2509f63c2497d7c32c372a449ff725
Nicolas Mayorga <namayorgav@gmail.com>
2024-08-27 21:00:09 -0500

    feat($auth): add middleware checking for routes and links displaying

--- src/components/navbar/LinksDropdown.tsx
+++ src/components/navbar/LinksDropdown.tsx
@@ -12,8 +12,12 @@ import Link from "next/link"
 import { SignedIn, SignedOut, SignInButton, SignUpButton } from "@clerk/nextjs"
 import SignOutLink from "./SignOutLink"
 import UserIcon from "./UserIcon"
+import { auth } from "@clerk/nextjs/server"
 
 function LinksDropdown() {
+  const { userId } = auth()
+  const isAdmin = userId === process.env.ADMIN_USER_ID
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -39,6 +43,8 @@ function LinksDropdown() {
 
         <SignedIn>
           {links.map((link) => {
+            if (link.label === "dashboard" && !isAdmin) return null
+
             return (
               <DropdownMenuItem key={link.href}>
                 <Link href={link.href} className="capitalize w-full">
