b803b14b88412fff31f79c02d3e0c3b3777d077d
Nicolas Mayorga <namayorgav@gmail.com>
2024-08-27 20:59:48 -0500

    feat($admin): add dashboard view

--- src/app/admin/Sidebar.tsx
+++ src/app/admin/Sidebar.tsx
@@ -1,15 +1,28 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
 import { adminLinks } from "@/utils/links"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 function Sidebar() {
+  const pathname = usePathname()
+
   return (
     <aside>
       {adminLinks.map((link) => {
         const { href, label } = link
 
+        const isActivePage = pathname === href
+        const classVariant = isActivePage ? "default" : "ghost"
+
         return (
-          <Button key={href} asChild>
+          <Button
+            key={href}
+            variant={classVariant}
+            className="w-full mb-2 capitalize font-normal justify-start"
+            asChild
+          >
             <Link href={href}>{label}</Link>
           </Button>
         )
