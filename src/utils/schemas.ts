6a11daa3bd1db0d895ee7011081d13883854626e
Nicolas Mayorga <namayorgav@gmail.com>
2024-08-29 12:06:10 -0500

    chore($actions): add supabase bucket for image upload.

--- src/utils/schemas.ts
+++ src/utils/schemas.ts
@@ -4,15 +4,15 @@ export const productSchema = z.object({
   name: z
     .string()
     .min(2, {
-      message: "name must be at least 2 characters.",
+      message: "name must be at least 2 characters",
     })
     .max(100, {
-      message: "name must be less than 100 characters.",
+      message: "name must be less than 100 characters",
     }),
   company: z.string(),
   featured: z.coerce.boolean(),
   price: z.coerce.number().int().min(0, {
-    message: "price must be a positive number.",
+    message: "price must be a positive number",
   }),
   description: z.string().refine(
     (description) => {
@@ -20,7 +20,46 @@ export const productSchema = z.object({
       return wordCount >= 10 && wordCount <= 1000
     },
     {
-      message: "description must be between 10 and 1000 words.",
+      message: "description must be between 10 and 1000 words",
     }
   ),
 })
+
+export function validateWithZodSchema<T>(
+  schema: ZodSchema<T>,
+  data: unknown
+): T {
+  const result = schema.safeParse(data)
+
+  if (!result.success) {
+    const errors = result.error.errors.map((error) => error.message)
+    throw new Error(errors.join(", "))
+  }
+
+  return result.data
+}
+
+export const imageSchema = z.object({
+  image: validateImageFile(),
+})
+
+function validateImageFile() {
+  const maxUploadSize = 1024 * 1024
+  const acceptedFileTypes = ["image/"]
+
+  return z
+    .instanceof(File)
+    .refine(
+      (file) => {
+        return !file || file.size <= maxUploadSize
+      },
+      {
+        message: "image must be less than 1MB",
+      }
+    )
+    .refine((file) => {
+      return (
+        !file || acceptedFileTypes.some((type) => file.type.startsWith(type))
+      )
+    })
+}
